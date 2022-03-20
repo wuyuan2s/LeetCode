02368deb40ab99cef0b7ea7be81272284dd77e72
wuyuan <1563829433@qq.com>
2022-03-21 00:26:47 +0800

    add: 第53题 ---简单题

--- 51-60/src/MaxSubArray.java
+++ 51-60/src/MaxSubArray.java
@@ -11,13 +11,13 @@ public class MaxSubArray {
     public static int maxSubArray(int[] nums) {
         int max = nums[0];
         int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-           if (sum > 0){
-               sum += nums[i];
-           } else{
-               sum = nums[i];
-           }
-           max = Math.max(max, sum);
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            max = Math.max(max, sum);
         }
         return max;
     }
