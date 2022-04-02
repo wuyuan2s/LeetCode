6e63da1741e4df12f88af4a1ccf77539ffa4792b
wuyuan <1563829433@qq.com>
2022-04-02 23:28:21 +0800

    add: 整数转罗马数字

---
+++ 1-50/src/IntToRoman.java
@@ -0,0 +1,27 @@
+public class IntToRoman {
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(58));
+        System.out.println(intToRoman(3));
+    }
+
+    public static String intToRoman(int num) {
+        int[] array;
+        String[] strs;
+        array = new int[]{
+                1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        strs = new String[]{
+                "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        };
+        StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < array.length; i++) {
+                while (num >= array[i]) {
+                    num -= array[i];
+                    result.append(strs[i]);
+                }
+            }
+
+        return result.toString();
+    }
+}
